6c4d325471a415b01a5ecbc834fb65183bab0620
JustSomeSoggybread <evanhlee@outlook.com>
2021-07-06 14:44:28 -0700

    first commit

---
+++ src/Lab10Master.ino
@@ -0,0 +1,41 @@
+SYSTEM_MODE(MANUAL);
+SYSTEM_THREAD(ENABLED);
+
+#include <Wire.h>
+#include "oled-wing-adafruit.h"
+
+
+/*
+      ## MASTER ##
+*/
+void setup() {
+  // Put initialization like pinMode and begin functions here.
+  Serial.begin(9600);
+
+  
+  //char value = Serial.read();
+
+  while (!Serial.isConnected());
+
+  Wire.begin();
+}
+
+void loop() {
+  // The core of your code will likely live here.
+  
+  if (Serial.available() > 0){
+    char inputC = Serial.read();
+
+    Wire.beginTransmission(0x2A);
+    Wire.write(inputC);
+    Wire.endTransmission();
+
+    delay(1000);
+
+    Wire.requestFrom(0x2A, 1);
+    //while (Wire.available() == 0);
+    char value = Wire.read();
+
+    Serial.println(value);
+  }
+}
